60a12e25cac61f8ffe97ea30d4135c64865c6cf8
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-11-01 21:32:15 +0600

    Loader

--- src/assets/Common/Loader/Loader.jsx
+++ src/assets/Common/Loader/Loader.jsx
@@ -6,7 +6,7 @@ const Loader = () => {
             <div role="status">
                 <svg
                     aria-hidden="true"
-                    className="inline w-10 h-10 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-cyan-500"
+                    className="inline w-10 h-10 mr-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-500"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
