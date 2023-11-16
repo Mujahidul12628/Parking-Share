c19a27ec86962cb38af47857c6fa4f272c5fee63
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-11-16 23:37:31 +0600

    Payment Data

---
+++ src/assets/Pages/Payment/DataOfPayment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataOfPayment = () => {
+    return (
+        <div>
+            <h1>Data of Payment</h1>
+        </div>
+    );
+};
+
+export default DataOfPayment;
